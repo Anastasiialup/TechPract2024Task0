6b7f95779d4bbf4e0c723795de21d1b6ada068f1
anastasiia <vt221_laa@student.ztu.edu.ua>
2024-02-21 20:37:10 +0200

    Add sum programm

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Please enter two numbers separated by a space: ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int num1 = Int32.Parse(numbers[0]);
+            int num2 = Int32.Parse(numbers[1]);
+            int sum = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} equals {sum}.");
+        }
+    }
+}
